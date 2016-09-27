75cff24c9b1fa4bea06f76cadbd301b630e6815a
Gen <archiherewego@gmail.com>
2016-09-27 21:26:12 +0900

    Add custom application class

---
+++ app/src/main/java/jp/gcreate/sample/daggersandbox/MyApplication.java
@@ -0,0 +1,14 @@
+package jp.gcreate.sample.daggersandbox;
+
+import android.app.Application;
+
+/**
+ * Copyright 2016 G-CREATE
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
