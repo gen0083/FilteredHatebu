83543cd483d185dcbe2bea8cadfb0852dcd02318
Gen <archiherewego@gmail.com>
2016-09-30 21:48:01 +0900

    Update logging on okhttp client

--- app/src/test/java/jp/gcreate/sample/daggersandbox/RetrofitTest.java
+++ app/src/test/java/jp/gcreate/sample/daggersandbox/RetrofitTest.java
@@ -8,10 +8,7 @@ import java.util.concurrent.CountDownLatch;
 
 import jp.gcreate.sample.daggersandbox.api.HatebuService;
 import jp.gcreate.sample.daggersandbox.model.HatebuEntry;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.ResponseBody;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,23 +27,17 @@ public class RetrofitTest {
 
     @Before
     public void setUp() {
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new Interceptor() {
+        HttpLoggingInterceptor logger = new HttpLoggingInterceptor(
+                new HttpLoggingInterceptor.Logger() {
                     @Override
-                    public okhttp3.Response intercept(Chain chain) throws IOException {
-                        Request request = chain.request();
-
-                        okhttp3.Response response = chain.proceed(request);
-                        String           rawJson  = response.body().string();
-
-                        System.out.println("request:" + request);
-                        System.out.println(String.format("raw JSON response is: %s", rawJson));
-
-                        return response.newBuilder().body(
-                                ResponseBody.create(response.body().contentType(), rawJson)).build();
+                    public void log(String message) {
+                        System.out.println(message);
                     }
                 })
-                .addNetworkInterceptor(new HttpLoggingInterceptor())
+                .setLevel(HttpLoggingInterceptor.Level.HEADERS);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addNetworkInterceptor(logger)
+                .addInterceptor(logger)
                 .build();
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
