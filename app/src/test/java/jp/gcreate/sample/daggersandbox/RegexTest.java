dcbfd4bcfd775d6ab6ecdbb57c68c9e731e63f32
Gen <archiherewego@gmail.com>
2016-10-11 21:26:15 +0900

    Add filter generator

--- app/src/test/java/jp/gcreate/sample/daggersandbox/RegexTest.java
+++ app/src/test/java/jp/gcreate/sample/daggersandbox/RegexTest.java
@@ -34,23 +34,30 @@ public class RegexTest {
 
     @Test
     public void ドメイン名を抽出する() {
-        Pattern p = Pattern.compile("http://(.+?)/");
+        Pattern p = Pattern.compile("http://(.+?/)");
         Matcher m = p.matcher("http://www.hoge.com/fuga/something");
         if (m.find()) {
             assertThat(m.groupCount(), is(1));
-            assertThat(m.group(1), is("www.hoge.com"));
+            assertThat(m.group(1), is("www.hoge.com/"));
         }
 
-        m = p.matcher("http://fuga.com/hoge/something");
+        m = p.matcher("http://fuga.com/hoge/test/some/any/something");
         m.find();
-        assertThat(m.group(1), is("fuga.com"));
+        assertThat(m.group(1), is("fuga.com/"));
     }
 
     @Test
     public void ドメイン名_サブディレクトリを抽出する() {
-        Pattern p = Pattern.compile("http://(.+?/.+?)/");
-        Matcher m = p.matcher("http://hoge.com/test/index.html");
+        Pattern p = Pattern.compile("http://(.+?/.+?/)");
+        Matcher m = p.matcher("http://hoge.com/test/abc/def/index.html");
         m.find();
-        assertThat(m.group(1), is("hoge.com/test"));
+        assertThat(m.group(1), is("hoge.com/test/"));
+    }
+
+    @Test
+    public void サブドメインを消す() {
+        String target = "www.hoge.fuga.co.jp/";
+        String[] splited = target.split("\\.");
+        assertThat(splited.length, is(5));
     }
 }
