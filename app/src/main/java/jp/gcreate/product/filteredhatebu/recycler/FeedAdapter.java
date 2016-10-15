716e67f123c6dbea6e84eff5736efffe77dc57b6
Gen <archiherewego@gmail.com>
2016-10-15 16:34:08 +0900

    Change log level debug to info

--- app/src/main/java/jp/gcreate/product/filteredhatebu/recycler/FeedAdapter.java
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/recycler/FeedAdapter.java
@@ -32,13 +32,13 @@ public class FeedAdapter extends RecyclerView.Adapter<DataBindingViewHolder<Item
     public FeedAdapter(Context context, List<HatebuFeedItem> items) {
         this.context = context;
         this.items = items;
-        Timber.d("%s constructor called.", this);
+        Timber.i("%s constructor called.", this);
     }
 
     @Override
     public DataBindingViewHolder<ItemHatebuFeedBinding> onCreateViewHolder(ViewGroup parent,
                                                                            int viewType) {
-        Timber.d("%s onCreateViewHolder", this);
+        Timber.i("%s onCreateViewHolder", this);
         View view = LayoutInflater.from(context).inflate(R.layout.item_hatebu_feed, parent, false);
         view.setOnClickListener(this);
         return new DataBindingViewHolder<>(view);
@@ -47,7 +47,7 @@ public class FeedAdapter extends RecyclerView.Adapter<DataBindingViewHolder<Item
     @Override
     public void onBindViewHolder(DataBindingViewHolder<ItemHatebuFeedBinding> holder,
                                  int position) {
-        Timber.d("%s onBindViewHolder", this);
+        Timber.i("%s onBindViewHolder", this);
         ItemHatebuFeedBinding binding = holder.getBinding();
         final HatebuFeedItem  item    = items.get(position);
         binding.setItem(item);
@@ -57,7 +57,7 @@ public class FeedAdapter extends RecyclerView.Adapter<DataBindingViewHolder<Item
     @Override
     public int getItemCount() {
         int count = items.size();
-        Timber.d("%s getItemCount:%d", this, count);
+        Timber.i("%s getItemCount:%d", this, count);
         return count;
     }
 
@@ -69,7 +69,7 @@ public class FeedAdapter extends RecyclerView.Adapter<DataBindingViewHolder<Item
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
-        Timber.d("%s onAttachedRecyclerView:%s", this, recyclerView);
+        Timber.i("%s onAttachedRecyclerView:%s", this, recyclerView);
         this.recyclerView = recyclerView;
         this.context = recyclerView.getContext();
     }
@@ -77,19 +77,19 @@ public class FeedAdapter extends RecyclerView.Adapter<DataBindingViewHolder<Item
     @Override
     public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
         super.onDetachedFromRecyclerView(recyclerView);
-        Timber.d("%s onDetachedFromRecyclerView:%s", this, recyclerView);
+        Timber.i("%s onDetachedFromRecyclerView:%s", this, recyclerView);
         this.recyclerView = null;
         this.context = null;
     }
 
     @Override
     public void onClick(View v) {
-        Timber.d("%s onClick view:%s", this, v);
+        Timber.i("%s onClick view:%s", this, v);
         int                  position = recyclerView.getChildAdapterPosition(v);
         final HatebuFeedItem item     = items.get(position);
-        Timber.d("%s onClick position:%d", this, position);
+        Timber.i("%s onClick position:%d", this, position);
         if (listener != null) {
-            Timber.d("%s onClick: callback to %s", this, listener);
+            Timber.i("%s onClick: callback to %s", this, listener);
             listener.onClick(this, position, item);
         }
     }
