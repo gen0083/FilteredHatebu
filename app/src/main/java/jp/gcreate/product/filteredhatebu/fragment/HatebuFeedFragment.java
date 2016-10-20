6fb9577891a73b194ef0abefab3274a52f6bfbfb
Gen <archiherewego@gmail.com>
2016-10-20 11:41:23 +0900

    Add divider to feed list

--- app/src/main/java/jp/gcreate/product/filteredhatebu/fragment/HatebuFeedFragment.java
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/fragment/HatebuFeedFragment.java
@@ -7,6 +7,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -110,6 +111,7 @@ public class HatebuFeedFragment extends Fragment implements FeedAdapter.OnRecyce
         layoutManager = new LinearLayoutManager(context);
         binding.recyclerView.setLayoutManager(layoutManager);
         binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
     }
 
     @Override
