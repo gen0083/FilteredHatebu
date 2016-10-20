a599d09f407c479a9e660cd02319997b0c2ccc71
Gen <archiherewego@gmail.com>
2016-10-20 22:57:07 +0900

    Show delete action when item swiping

--- app/src/main/java/jp/gcreate/product/filteredhatebu/recycler/FilterAdapter.java
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/recycler/FilterAdapter.java
@@ -1,6 +1,11 @@
 package jp.gcreate.product.filteredhatebu.recycler;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
@@ -22,10 +27,11 @@ public class FilterAdapter extends RecyclerView.Adapter<DataBindingViewHolder<It
     private Context         context;
     private List<UriFilter> list;
     private RecyclerView    recyclerView;
-    private ItemTouchHelper touchHelper = new SwipeDismissTouchHelper();
+    private ItemTouchHelper touchHelper;
 
     public FilterAdapter(Context context) {
         this.context = context;
+        touchHelper = new SwipeDismissTouchHelper();
         list = new ArrayList<>();
     }
 
@@ -70,6 +76,25 @@ public class FilterAdapter extends RecyclerView.Adapter<DataBindingViewHolder<It
     private class SwipeDismissTouchHelper extends ItemTouchHelper {
         public SwipeDismissTouchHelper() {
             this(new ItemTouchHelper.SimpleCallback(0, LEFT | RIGHT) {
+                private Drawable deleteIcon;
+                private Drawable background;
+                private int padding;
+                private Paint paint;
+                private int iconWidth;
+                private int iconHeight;
+                private int color;
+                private boolean isInitialized = false;
+
+                private void init() {
+                    deleteIcon = ContextCompat.getDrawable(context, R.drawable.ic_delete);
+                    paint = new Paint();
+                    color = ContextCompat.getColor(context, R.color.colorAccent);
+                    background = new ColorDrawable(color);
+                    padding = (int) (context.getResources().getDisplayMetrics().density * 16);
+                    iconWidth = deleteIcon.getIntrinsicWidth();
+                    iconHeight = deleteIcon.getIntrinsicHeight();
+                    isInitialized = true;
+                }
 
                 @Override
                 public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
@@ -81,6 +106,34 @@ public class FilterAdapter extends RecyclerView.Adapter<DataBindingViewHolder<It
                 public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
                     int position = viewHolder.getAdapterPosition();
                     list.remove(position);
+                }
+
+                @Override
+                public void onChildDraw(Canvas c, RecyclerView recyclerView,
+                                        RecyclerView.ViewHolder viewHolder,
+                                        float dX, float dY, int actionState,
+                                        boolean isCurrentlyActive) {
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState,
+                                      isCurrentlyActive);
+                    if (dX == 0) return;
+                    if (!isInitialized) {
+                        init();
+                    }
+                    int left  = dX > 0 ? 0        : (int) dX;
+                    int right = dX > 0 ? (int) dX : viewHolder.itemView.getRight();
+                    int top = viewHolder.itemView.getTop();
+                    int bottom = viewHolder.itemView.getBottom();
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(c);
+
+                    int margin = (bottom - top - iconHeight) / 2;
+                    int dTop = top + margin;
+                    int dBottom = bottom - margin;
+                    int dLeft = dX > 0 ? padding : right - padding - iconWidth;
+                    int dRight = dX > 0 ? padding + iconWidth : right - padding;
+
+                    deleteIcon.setBounds(dLeft, dTop, dRight, dBottom);
+                    deleteIcon.draw(c);
                 }
             });
         }
