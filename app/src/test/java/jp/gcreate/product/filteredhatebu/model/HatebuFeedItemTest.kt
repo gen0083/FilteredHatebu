a654538301c7bd0f7380cbc4899a66d4117097ef
Gen <archiherewego@gmail.com>
2018-06-20 00:13:03 +0900

    Fix failed parse HatebuFeedItem with error
    error:
    org.simpleframework.xml.core.PersistenceException: Element 'subject' is already used with @org.simpleframework.xml.Element
    あるタイミングからおそらくsubjectが複数帰るようになってパースできなくなったものと考えられる
    https://stackoverflow.com/questions/31999265/parsing-xml-feed-die-with-element-is-already-used
    ここの対策をとったらパースエラーでなくなった
    おそらく複数あるsubjectを最初の1個パースして、さらに追加でsubjectが出てきた場合は無視するような設定なのだと思う

---
+++ app/src/test/java/jp/gcreate/product/filteredhatebu/model/HatebuFeedItemTest.kt
@@ -0,0 +1,165 @@
+package jp.gcreate.product.filteredhatebu.model
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.simpleframework.xml.core.Persister
+import java.io.File
+
+/**
+ * Copyright 2016 G-CREATE
+ */
+class HatebuFeedItemTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun equals_same_case() {
+        val a = HatebuFeedItem()
+        val b = HatebuFeedItem()
+        // field about is required
+        a.about = "a"
+        b.about = "a"
+        // field title is required
+        a.title = "a"
+        b.title = "a"
+        // field link is required
+        a.link = "a"
+        b.link = "a"
+        // field description is not required
+        a.description = "a"
+        b.description = "a"
+        // field encoded is required
+        a.encoded = "a"
+        b.encoded = "a"
+        // field date is required
+        a.date = "a"
+        b.date = "a"
+        // field subject is not required
+        a.subject = "a"
+        b.subject = "a"
+        // field count is required
+        a.count = 1
+        b.count = 1
+
+        assertThat(a == b, `is`(true))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun equals_same_case_with_null() {
+        val a = HatebuFeedItem()
+        val b = HatebuFeedItem()
+        // field about is required
+        a.about = "a"
+        b.about = "a"
+        // field title is required
+        a.title = "a"
+        b.title = "a"
+        // field link is required
+        a.link = "a"
+        b.link = "a"
+        // field description is not required
+        a.description = null
+        b.description = null
+        // field encoded is required
+        a.encoded = "a"
+        b.encoded = "a"
+        // field date is required
+        a.date = "a"
+        b.date = "a"
+        // field subject is not required
+        a.subject = null
+        b.subject = null
+        // field count is required
+        a.count = 1
+        b.count = 1
+
+        assertThat(a == b, `is`(true))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun equals_different_case() {
+        val a = HatebuFeedItem()
+        val b = HatebuFeedItem()
+        // field about is required
+        a.about = "a"
+        b.about = "a"
+        // field title is required
+        a.title = "a"
+        b.title = "a"
+        // field link is required
+        a.link = "a"
+        b.link = "a"
+        // field description is not required
+        a.description = "a"
+        b.description = "a"
+        // field encoded is required
+        a.encoded = "a"
+        b.encoded = "ab"
+        // field date is required
+        a.date = "a"
+        b.date = "a"
+        // field subject is not required
+        a.subject = "a"
+        b.subject = "a"
+        // field count is required
+        a.count = 1
+        b.count = 1
+
+        assertThat(a == b, `is`(false))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun equals_different_case_with_null() {
+        val a = HatebuFeedItem()
+        val b = HatebuFeedItem()
+        // field about is required
+        a.about = "a"
+        b.about = "a"
+        // field title is required
+        a.title = "a"
+        b.title = "a"
+        // field link is required
+        a.link = "a"
+        b.link = "a"
+        // field description is not required
+        a.description = null
+        b.description = "a"
+        // field encoded is required
+        a.encoded = "a"
+        b.encoded = "a"
+        // field date is required
+        a.date = "a"
+        b.date = "a"
+        // field subject is not required
+        a.subject = "a"
+        b.subject = "a"
+        // field count is required
+        a.count = 1
+        b.count = 1
+
+        assertThat(a == b, `is`(false))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun decode() {
+        val serializer = Persister()
+        val file = File(javaClass.classLoader.getResource("mock_hatebu_hotentry.rss").file)
+        val feed = serializer.read(HatebuFeed::class.java, file)
+        val actual = feed.itemList[0]
+        assertThat(actual.title, `is`("test0-1"))
+        assertThat(actual.link, `is`("http://test.com/hoge"))
+    }
+    
+    @Test fun `new condition decode xml`() {
+        // あるタイミングからおそらくdc:subject（タグ）が複数出力されるようになった
+        val serializer = Persister()
+        val file = File(javaClass.classLoader.getResource("feedburner_hotentry.xml").file)
+        val feed = serializer.read(HatebuFeed::class.java, file)
+        val actual = feed.itemList[2]
+        print(actual)
+    }
+}
