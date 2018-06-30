86c21d48693b62024de82d830d0dc7763ed9404b
Gen <archiherewego@gmail.com>
2018-06-30 11:24:40 +0900

    Update deploy shell

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,10 @@ echo $TRAVIS_PULL_REQUEST
 git --version
 
 pwd
-./gradlew bumpVersion
-./gradlew :app:assembleProdRelease
+VERSION=`./gradlew bumpVersion | grep -E "^v\d+\.\d+\.\d+"`
+echo "$VERSION"
 ./gradlew :app:publishProdRelease
+git add --all
+git commit -m "Released $VERSION by CI"
+git tag "$VERSION"
+git push origin master
