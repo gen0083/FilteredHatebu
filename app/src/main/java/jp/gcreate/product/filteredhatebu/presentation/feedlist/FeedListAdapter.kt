f1429b16044dc455c809f42c61bf1585f3b4d528
Gen <archiherewego@gmail.com>
2018-07-10 14:48:22 +0900

    Change to stop fetching hatebu count on every shown
    毎回はてブカウント拾いに行くのをやめた
    （無駄が多すぎる）

--- app/src/main/java/jp/gcreate/product/filteredhatebu/presentation/feedlist/FeedListAdapter.kt
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/presentation/feedlist/FeedListAdapter.kt
@@ -9,21 +9,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import jp.gcreate.product.filteredhatebu.R
-import jp.gcreate.product.filteredhatebu.api.HatenaClient
 import jp.gcreate.product.filteredhatebu.data.entities.FeedData
 import jp.gcreate.product.filteredhatebu.databinding.ItemFeedListItemBinding
 import jp.gcreate.product.filteredhatebu.di.Scope.ActivityScope
 import jp.gcreate.product.filteredhatebu.ui.common.HandleOnceEvent
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
-import rx.subscriptions.SerialSubscription
-import timber.log.Timber
 import javax.inject.Inject
 
 @ActivityScope
-class FeedListAdapter @Inject constructor(
-    private val client: HatenaClient.JsonService
-) : ListAdapter<FeedData, FeedListViewHolder>(
+class FeedListAdapter @Inject constructor() : ListAdapter<FeedData, FeedListViewHolder>(
     object : DiffUtil.ItemCallback<FeedData>() {
         override fun areItemsTheSame(oldItem: FeedData, newItem: FeedData): Boolean {
             return oldItem.url == newItem.url
@@ -38,10 +31,8 @@ class FeedListAdapter @Inject constructor(
     val clickEvent: LiveData<HandleOnceEvent<FeedData>> = clickEventSender
     
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedListViewHolder {
-        return FeedListViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_feed_list_item, parent, false),
-            client
-        )
+        return FeedListViewHolder(LayoutInflater.from(parent.context)
+                                      .inflate(R.layout.item_feed_list_item, parent, false))
     }
     
     override fun onBindViewHolder(holder: FeedListViewHolder, position: Int) {
@@ -53,24 +44,11 @@ class FeedListAdapter @Inject constructor(
     }
 }
 
-class FeedListViewHolder(
-    itemView: View,
-    private val client: HatenaClient.JsonService
-) : RecyclerView.ViewHolder(itemView) {
+class FeedListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     
-    private val subscription = SerialSubscription()
     private val binding: ItemFeedListItemBinding = ItemFeedListItemBinding.bind(itemView)
     
     fun bind(feedData: FeedData) {
         binding.item = feedData
-        subscription.set(
-            client.getEntry(feedData.url)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { binding.count.text = "${it.count}" },
-                    { Timber.e(it) }
-                )
-        )
     }
 }
