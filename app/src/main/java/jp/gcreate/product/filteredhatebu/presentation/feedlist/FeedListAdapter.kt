c16771c219f895443ec3eeca17d85a5c7d8559c6
Gen <archiherewego@gmail.com>
2018-07-10 16:44:43 +0900

    Show favicon

--- app/src/main/java/jp/gcreate/product/filteredhatebu/presentation/feedlist/FeedListAdapter.kt
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/presentation/feedlist/FeedListAdapter.kt
@@ -8,15 +8,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.reactivex.disposables.SerialDisposable
 import jp.gcreate.product.filteredhatebu.R
 import jp.gcreate.product.filteredhatebu.data.entities.FeedData
 import jp.gcreate.product.filteredhatebu.databinding.ItemFeedListItemBinding
 import jp.gcreate.product.filteredhatebu.di.Scope.ActivityScope
+import jp.gcreate.product.filteredhatebu.ui.common.FaviconUtil
 import jp.gcreate.product.filteredhatebu.ui.common.HandleOnceEvent
+import timber.log.Timber
 import javax.inject.Inject
 
 @ActivityScope
-class FeedListAdapter @Inject constructor() : ListAdapter<FeedData, FeedListViewHolder>(
+class FeedListAdapter @Inject constructor(private val faviconUtil: FaviconUtil)
+    : ListAdapter<FeedData, FeedListViewHolder>(
     object : DiffUtil.ItemCallback<FeedData>() {
         override fun areItemsTheSame(oldItem: FeedData, newItem: FeedData): Boolean {
             return oldItem.url == newItem.url
@@ -27,12 +31,14 @@ class FeedListAdapter @Inject constructor() : ListAdapter<FeedData, FeedListView
         }
     }
 ) {
+    
     private val clickEventSender: MutableLiveData<HandleOnceEvent<FeedData>> = MutableLiveData()
     val clickEvent: LiveData<HandleOnceEvent<FeedData>> = clickEventSender
     
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedListViewHolder {
         return FeedListViewHolder(LayoutInflater.from(parent.context)
-                                      .inflate(R.layout.item_feed_list_item, parent, false))
+                                      .inflate(R.layout.item_feed_list_item, parent, false),
+                                  faviconUtil)
     }
     
     override fun onBindViewHolder(holder: FeedListViewHolder, position: Int) {
@@ -44,12 +50,21 @@ class FeedListAdapter @Inject constructor() : ListAdapter<FeedData, FeedListView
     }
 }
 
-class FeedListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class FeedListViewHolder(itemView: View, private val faviconUtil: FaviconUtil)
+    : RecyclerView.ViewHolder(itemView) {
     
+    private val disposable = SerialDisposable()
     private val binding: ItemFeedListItemBinding = ItemFeedListItemBinding.bind(itemView)
     
     fun bind(feedData: FeedData) {
         binding.item = feedData
         binding.executePendingBindings()
+        disposable.set(
+            faviconUtil.fetchFaviconAsSingle(feedData.url)
+                .subscribe(
+                    { binding.favicon.setImageDrawable(it) },
+                    { Timber.e(it) }
+                )
+        )
     }
 }
