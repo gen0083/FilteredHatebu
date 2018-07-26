324a16ee947f650d3100f8de077f3699cbacf0ad
Gen <archiherewego@gmail.com>
2018-07-26 10:28:23 +0900

    Update position drawing header
    ヘッダーが描画される位置を調整

--- app/src/main/java/jp/gcreate/product/filteredhatebu/ui/common/StickyHeaderDecoration.kt
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/ui/common/StickyHeaderDecoration.kt
@@ -13,6 +13,9 @@ import android.view.View
 import jp.gcreate.product.filteredhatebu.R
 import timber.log.Timber
 
+/**
+ * Note: This class supports LinearLayoutManager only.
+ */
 class StickyHeaderDecoration(context: Context, private val groupCallback: GroupCallback)
     : RecyclerView.ItemDecoration() {
     
@@ -20,8 +23,12 @@ class StickyHeaderDecoration(context: Context, private val groupCallback: GroupC
     private val backgroundPaint = Paint()
     private val headerPadding: Int
     private val headerMargin: Int
+    // ヘッダーの高さ（背景の装飾を含めたヘッダーとして描画されるオブジェクトの高さ）
     private val headerHeight: Int
-    private val fontMetrics: Paint.FontMetrics
+    // 文字のみの高さ
+    private val textHeight: Int
+    // ヘッダーの描画領域としてあける空間の高さ
+    private val headerAreaOffset: Int
     
     init {
         val resource = context.resources
@@ -35,37 +42,38 @@ class StickyHeaderDecoration(context: Context, private val groupCallback: GroupC
         
         backgroundPaint.apply {
             color = ContextCompat.getColor(context, R.color.colorPrimaryLight)
-            alpha = 100
+            alpha = 127
         }
+        val fontMetrics = textPaint.fontMetrics
         
-        fontMetrics = textPaint.fontMetrics
-    
         headerPadding = resource.getDimensionPixelSize(R.dimen.sticky_header_padding)
-        headerMargin = resource.getDimensionPixelSize(R.dimen.sticky_header_margin)
-    
-        headerHeight = (fontMetrics.run { leading - ascent } + (headerPadding + headerMargin) * 2).toInt()
+        headerMargin = resource.getDimensionPixelSize(R.dimen.item_padding)
+        
+        textHeight = fontMetrics.run { leading - ascent }.toInt()
+        headerHeight = textHeight + headerPadding * 2
+        // 各アイテムの上部にpaddingがあり、offsetでは上部のmarginのみを考慮すれば良い
+        headerAreaOffset = headerHeight + headerMargin
     }
     
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView,
                                 state: RecyclerView.State) {
         val position = parent.getChildAdapterPosition(view)
         if (groupCallback.isBoundary(position)) {
-            outRect.top = headerHeight
+            outRect.top = headerAreaOffset
         }
     }
     
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         super.onDraw(c, parent, state)
         // yLimitはRecyclerViewの一番上の位置にHeaderを固定するときの位置
-        val yLimit = headerHeight - headerPadding - headerMargin
-        val lineHeight = fontMetrics.run { leading - ascent }
+        val yLimit = headerMargin + textHeight
         var previousHeader = ""
         val layoutManager = parent.layoutManager as LinearLayoutManager
         val firstPosition = layoutManager.findFirstVisibleItemPosition()
         val lastPosition = layoutManager.findLastVisibleItemPosition()
         Timber.v("onDraw firstVisible position: $firstPosition/$lastPosition")
         val textX = (parent.width / 2).toFloat()
-    
+        
         if (firstPosition < 0 || lastPosition < 0) return
         for (i in firstPosition..lastPosition) {
             val view = parent.findViewHolderForAdapterPosition(i).itemView
@@ -73,27 +81,39 @@ class StickyHeaderDecoration(context: Context, private val groupCallback: GroupC
             val headerText = groupCallback.getGroupHeaderText(i)
             if (previousHeader != headerText) {
                 // draw header text
-                val viewTop = view.top + headerMargin + headerPadding
+                val viewTop = view.top - headerPadding
                 val textY = if (groupCallback.isBoundary(i)) {
-                    Math.max(yLimit, viewTop - headerPadding - headerMargin).toFloat()
+                    Math.max(yLimit, viewTop).toFloat()
                 } else {
                     yLimit.toFloat()
                 }
-                c.drawText(headerText, textX, textY, textPaint)
                 val textLength = textPaint.measureText(headerText) / 2
                 val rectF = RectF(textX - textLength - headerPadding,
-                                  textY - lineHeight - headerPadding,
+                                  textY - textHeight - headerPadding,
                                   textX + textLength + headerPadding, textY + headerPadding)
                 c.drawRoundRect(rectF, 15f, 15f, backgroundPaint)
-        
+                c.drawText(headerText, textX, textY, textPaint)
+                
                 previousHeader = headerText
             }
         }
     }
     
     interface GroupCallback {
+        /**
+         * 所属するグループのIDを返す
+         */
         fun getGroupId(position: Int): Long
+        
+        /**
+         * 所属するグループとして表示するヘッダーの文字列を返す
+         */
         fun getGroupHeaderText(position: Int): String
+        
+        /**
+         * ヘッダーを挿入する境界となるViewかどうかを返す
+         * （List中の直前のアイテムとグループが異なればtrue, 直前のアイテムと同じならfalse）
+         */
         fun isBoundary(position: Int): Boolean
     }
 }
