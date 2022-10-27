a8fa6758313094c4da2af4317769cf6cc10de246
Gen <archiherewego@gmail.com>
2022-10-27 12:17:17 +0900

    Room / moshiの更新とそれに伴うコードの微修正
    主にRoomの警告への対応
    Moshiはバージョンを上げただけ

--- app/src/main/java/jp/gcreate/product/filteredhatebu/data/AppRoomDatabase.kt
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/data/AppRoomDatabase.kt
@@ -3,11 +3,7 @@ package jp.gcreate.product.filteredhatebu.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import jp.gcreate.product.filteredhatebu.data.dao.DeletedFeedDao
-import jp.gcreate.product.filteredhatebu.data.dao.FeedDataDao
-import jp.gcreate.product.filteredhatebu.data.dao.FeedFilterDao
-import jp.gcreate.product.filteredhatebu.data.dao.FilteredFeedDao
-import jp.gcreate.product.filteredhatebu.data.dao.WorkLogDao
+import jp.gcreate.product.filteredhatebu.data.dao.*
 import jp.gcreate.product.filteredhatebu.data.entities.DeletedFeed
 import jp.gcreate.product.filteredhatebu.data.entities.FeedData
 import jp.gcreate.product.filteredhatebu.data.entities.FeedFilter
@@ -17,7 +13,7 @@ import jp.gcreate.product.filteredhatebu.data.entities.debug.WorkLog
 @Database(
     entities = [FeedData::class, FilteredFeed::class, FeedFilter::class, DeletedFeed::class,
         WorkLog::class],
-    version = 6,
+    version = 7,
     exportSchema = true
 )
 @TypeConverters(LocalDateTimeConverter::class, ZonedDateTimeConverter::class)
