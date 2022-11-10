8cb3c51d88f2b17c4cc266843117f0fd1df2f252
Gen <archiherewego@gmail.com>
2022-11-10 11:05:46 +0900

    use own coroutine scope instead of GlobalScope

--- app/src/main/java/jp/gcreate/product/filteredhatebu/domain/services/ArchiveFeedService.kt
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/domain/services/ArchiveFeedService.kt
@@ -1,28 +1,32 @@
 package jp.gcreate.product.filteredhatebu.domain.services
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import jp.gcreate.product.filteredhatebu.data.AppRoomDatabase
 import jp.gcreate.product.filteredhatebu.ui.common.HandleOnceEvent
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 private typealias ArchiveEvent = HandleOnceEvent<String>
 
 class ArchiveFeedService(db: AppRoomDatabase) {
-    
+
     private val feedDataDao = db.feedDataDao()
-    private val archiveEventEmitter = MutableLiveData<ArchiveEvent>()
-    val archiveEvent: LiveData<ArchiveEvent> = archiveEventEmitter
+    private val archiveEventEmitter: MutableStateFlow<ArchiveEvent> =
+        MutableStateFlow(ArchiveEvent(""))
+    val archiveEvent: StateFlow<ArchiveEvent> = archiveEventEmitter
     private var archivedUrl: String? = null
-    
-    fun archiveFeed(url: String) = GlobalScope.launch {
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    fun archiveFeed(url: String) = scope.launch {
         feedDataDao.updateStatusArchived(url, true)
         archivedUrl = url
-        archiveEventEmitter.postValue(ArchiveEvent(url))
+        archiveEventEmitter.emit(ArchiveEvent(url))
     }
-    
-    fun undoArchive() = GlobalScope.launch {
+
+    fun undoArchive() = scope.launch {
         archivedUrl?.let {
             feedDataDao.updateStatusArchived(it, false)
             archivedUrl = null
