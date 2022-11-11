64e540e5ee0bf35d189a7458598ec76bee0d1f96
Gen <archiherewego@gmail.com>
2022-11-11 10:38:57 +0900

    update koin ViewModelModule

--- app/src/main/java/jp/gcreate/product/filteredhatebu/di/KoinViewModelModule.kt
+++ app/src/main/java/jp/gcreate/product/filteredhatebu/di/KoinViewModelModule.kt
@@ -5,13 +5,13 @@ import jp.gcreate.product.filteredhatebu.ui.feeddetail.FeedDetailViewModel
 import jp.gcreate.product.filteredhatebu.ui.feedlist.FeedListViewModel
 import jp.gcreate.product.filteredhatebu.ui.filterdetail.FilterDetailViewModel
 import jp.gcreate.product.filteredhatebu.ui.filterlist.FilterListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val koinViewModelModule = module {
-    viewModel { FavoriteViewModel(get()) }
-    viewModel { FeedDetailViewModel(get(), get(), get(), get()) }
-    viewModel { FeedListViewModel(get(), get(), get()) }
-    viewModel { FilterDetailViewModel(get(), get()) }
-    viewModel { FilterListViewModel(get(), get(), get()) }
+    viewModelOf(::FavoriteViewModel)
+    viewModelOf(::FeedDetailViewModel)
+    viewModelOf(::FeedListViewModel)
+    viewModelOf(::FilterDetailViewModel)
+    viewModelOf(::FilterListViewModel)
 }
